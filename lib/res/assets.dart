c1c85cb26d2de3fa7e5437f09a5cd3e00b6b1258
radnive <radnive.dev@gmail.com>
2022-06-28 09:38:38 -0700

    Add assets.

---
+++ lib/res/assets.dart
@@ -0,0 +1,134 @@
+class IconAssets {
+  // Icons Directory
+  static const String _iconsDir = 'assets/icons';
+  // Icons
+  static const String remixAddFill = '$_iconsDir/remix_add_fill.png';
+  static const String remixArrowDownFill = '$_iconsDir/remix_arrow_down_fill.png';
+  static const String remixDrop = '$_iconsDir/remix_drop.png';
+  static const String remixLeaf = '$_iconsDir/remix_leaf.png';
+  static const String remixSettings = '$_iconsDir/remix_settings.png';
+  static const String remixMenu = '$_iconsDir/remix_menu.png';
+  static const String remixMapPin = '$_iconsDir/remix_map_pin.png';
+  static const String remixLeftArrow = '$_iconsDir/remix_left_arrow.png';
+  static const String remixRightArrow = '$_iconsDir/remix_right_arrow.png';
+  static const String remixWind = '$_iconsDir/remix_wind.png';
+  static const String remixShowers = '$_iconsDir/remix_showers.png';
+  static const String remixSearch = '$_iconsDir/remix_search.png';
+  static const String remixLocation = '$_iconsDir/remix_location.png';
+  static const String remixCloudyFill = '$_iconsDir/remix_cloudy_fill.png';
+  static const String remixCloudyLine = '$_iconsDir/remix_cloudy_line.png';
+  static const String remixCompass = '$_iconsDir/remix_compass.png';
+  static const String remixDeleteBinFill = '$_iconsDir/remix_delete_bin_fill.png';
+  static const String remixSunLine = '$_iconsDir/remix_sun_line.png';
+  static const String remixTemperatureHotLine = '$_iconsDir/remix_temperature_hot_line.png';
+  static const String remixCloseFill = '$_iconsDir/remix_close_fill.png';
+  static const String remixSunriseLine = '$_iconsDir/remix_sunrise_line.png';
+  static const String remixSunsetLine = '$_iconsDir/remix_sunset_line.png';
+  static const String remixEyeLine = '$_iconsDir/remix_eye_line.png';
+  static const String remixPressureLine = '$_iconsDir/remix_pressure_line.png';
+  static const String remixAlertLine = '$_iconsDir/remix_alert_line.png';
+  static const String remixCheckDoubleLine = '$_iconsDir/remix_check_double_line.png';
+  static const String remixCloseCircleLine = '$_iconsDir/remix_close_circle_line.png';
+  static const String remixInformationLine = '$_iconsDir/remix_information_line.png';
+  static const String remixDribbbleLine = '$_iconsDir/remix_dribbble_line.png';
+  static const String remixGithubFill = '$_iconsDir/remix_github_fill.png';
+  static const String remixInstagramFill = '$_iconsDir/remix_instagram_fill.png';
+  static const String remixArrowDownCircleLine = '$_iconsDir/remix_arrow_down_circle_line.png';
+  static const String remixRocketLine = '$_iconsDir/remix_rocket_line.png';
+
+  static List<String> weatherDescribeIcons = [remixWind, remixSunLine, remixTemperatureHotLine, remixPressureLine, remixDrop, remixEyeLine];
+}
+
+class ImageAssets {
+  // Images Directory
+  static const String _imagesDir = 'assets/images';
+  // Images
+  // :: Piqo weather icons.
+  static const String dayCloudy = '$_imagesDir/piqo_weather_icons/day_cloudy.png';
+  static const String dayCloudyShowers = '$_imagesDir/piqo_weather_icons/day_cloudy_showers.png';
+  static const String dayCloudySnow = '$_imagesDir/piqo_weather_icons/day_cloudy_snow.png';
+  static const String dayCloudyThunder = '$_imagesDir/piqo_weather_icons/day_cloudy_thunder.png';
+  static const String dayDreary = '$_imagesDir/piqo_weather_icons/day_dreary.png';
+  static const String dayFlurries = '$_imagesDir/piqo_weather_icons/day_flurries.png';
+  static const String dayFog = '$_imagesDir/piqo_weather_icons/day_fog.png';
+  static const String dayFreezingRain = '$_imagesDir/piqo_weather_icons/day_freezing_rain.png';
+  static const String dayRain = '$_imagesDir/piqo_weather_icons/day_rain.png';
+  static const String dayRainySnowy = '$_imagesDir/piqo_weather_icons/day_rainy_snowy.png';
+  static const String dayShowers = '$_imagesDir/piqo_weather_icons/day_showers.png';
+  static const String daySleet = '$_imagesDir/piqo_weather_icons/day_sleet.png';
+  static const String daySnow = '$_imagesDir/piqo_weather_icons/day_snow.png';
+  static const String daySun = '$_imagesDir/piqo_weather_icons/day_sun.png';
+  static const String daySunFlurries = '$_imagesDir/piqo_weather_icons/day_sun_flurries.png';
+  static const String daySunHazy = '$_imagesDir/piqo_weather_icons/day_sun_hazy.png';
+  static const String daySunWindy = '$_imagesDir/piqo_weather_icons/day_sun_windy.png';
+  static const String dayThunder = '$_imagesDir/piqo_weather_icons/day_thunder.png';
+  static const String nightClear = '$_imagesDir/piqo_weather_icons/night_clear.png';
+  static const String nightCloudy = '$_imagesDir/piqo_weather_icons/night_cloudy.png';
+  static const String nightCloudyFlurries = '$_imagesDir/piqo_weather_icons/night_cloudy_flurries.png';
+  static const String nightCloudyShowers = '$_imagesDir/piqo_weather_icons/night_cloudy_showers.png';
+  static const String nightCloudySnow = '$_imagesDir/piqo_weather_icons/night_cloudy_snow.png';
+  static const String nightCloudyThunder = '$_imagesDir/piqo_weather_icons/night_cloudy_thunder.png';
+  static const String nightMoonHazy = '$_imagesDir/piqo_weather_icons/night_moon_hazy.png';
+  static const String unknownWeatherIcon = '$_imagesDir/piqo_weather_icons/unknown_weather_icon.png';
+  // :: Free images.
+  static const String lightEmptyList = '$_imagesDir/free_images/light_empty_list.png';
+  static const String lightNotFound = '$_imagesDir/free_images/light_not_found.png';
+  static const String darkEmptyList = '$_imagesDir/free_images/dark_empty_list.png';
+  static const String darkNotFound = '$_imagesDir/free_images/dark_not_found.png';
+  static const String amsterdam = '$_imagesDir/free_images/amsterdam.png';
+  static const String cryingFace = '$_imagesDir/free_images/crying_face.png';
+  static const String relievedFace = '$_imagesDir/free_images/relieved_face.png';
+  static const String smilingFace = '$_imagesDir/free_images/smiling_face.png';
+  static const String tiredFace = '$_imagesDir/free_images/tired_face.png';
+  static const String unamusedFace = '$_imagesDir/free_images/unamused_face.png';
+
+  static List<String> get weatherIcons => [
+    unknownWeatherIcon, // 0 <- UNKNOWN
+    daySun, // 1
+    dayCloudy, // 2
+    dayCloudy, // 3
+    dayCloudy, // 4
+    daySunHazy, // 5
+    dayCloudy, // 6
+    dayCloudy, // 7
+    dayDreary, // 8
+    unknownWeatherIcon, // 9 <- UNKNOWN
+    unknownWeatherIcon, // 10 <- UNKNOWN
+    dayFog, // 11
+    dayShowers, // 12
+    dayCloudyShowers, // 13
+    dayCloudyShowers, // 14
+    dayThunder, // 15
+    dayCloudyThunder, // 16
+    dayCloudyThunder, // 17
+    dayRain, // 18
+    dayFlurries, // 19
+    daySunFlurries, // 20
+    daySunFlurries, // 21
+    daySnow, // 22
+    dayCloudySnow, // 23
+    dayFreezingRain, // 24
+    daySleet, // 25
+    dayFreezingRain, // 26
+    unknownWeatherIcon, // 27 <- UNKNOWN
+    unknownWeatherIcon, // 28 <- UNKNOWN
+    dayRainySnowy, // 29
+    unknownWeatherIcon, // 30 <- UNKNOWN
+    unknownWeatherIcon, // 31 <- UNKNOWN
+    daySunWindy, // 32
+    nightClear, // 33
+    nightCloudy, // 34
+    nightCloudy, // 35
+    nightCloudy, // 36
+    nightMoonHazy, // 37
+    nightCloudy, // 38
+    nightCloudyShowers, // 39
+    nightCloudyShowers, // 40
+    nightCloudyThunder, // 41
+    nightCloudyThunder, // 42
+    nightCloudyFlurries, // 43
+    nightCloudySnow // 44
+  ];
+
+  static List<String> emojiIcons = [cryingFace, tiredFace, unamusedFace, relievedFace, smilingFace];
+}
