32ab414111d9005df2b82dffe21c4d7a5a48742e
radnive <radnive.dev@gmail.com>
2022-06-28 23:24:17 -0700

    Create Router.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:weather_app/database/database.dart';
+import 'package:weather_app/database/entities/settings_entity.dart';
 import 'package:weather_app/generated/l10n.dart';
 import 'package:weather_app/provider.dart';
 import 'package:weather_app/res/colors.dart';
 import 'package:weather_app/res/types.dart';
+import 'package:weather_app/router.dart';
 
+late AppRouterDelegate _routerDelegate;
+late AppRouteInformationParser _informationParser;
 late AppearanceChangeProvider _appearanceChangeProvider;
 
-void main() {
-  _appearanceChangeProvider = AppearanceChangeProvider();
-  runApp(const MyApp());
+void main() async {
+  // Required for getting application directory by ObjectBox to store database.
+  WidgetsFlutterBinding.ensureInitialized();
+  // Create database.
+  Database db = Database();
+  await db.create();
+  // Create router and information parser.
+  _routerDelegate = AppRouterDelegate();
+  _informationParser = AppRouteInformationParser();
+  // Load user settings into provider.
+  _loadUserSettings(db);
+  // Start app.
+  runApp(const WeatherApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// Load user settings into provider.
+void _loadUserSettings(Database db) {
+  // Check for saved settings.
+  if(Settings.isSettingsSaved(db)) {
+    // Get saved user settings.
+    Settings userSettings = Settings.get(db);
+    // Load settings into provider.
+    _appearanceChangeProvider = AppearanceChangeProvider(
+      language: userSettings.language,
+      themeMode: userSettings.themeMode
+    );
+  } else {
+    // Insert default settings to database.
+    Settings.insertDefaultSettings(db);
+    // Create provider with default settings.
+    _appearanceChangeProvider = AppearanceChangeProvider();
+  }
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +57,11 @@ class MyApp extends StatelessWidget {
       child: Builder(
         builder: (context) {
           final provider = Provider.of<AppearanceChangeProvider>(context, listen: true);
-          return MaterialApp(
+          return MaterialApp.router(
             title: 'Weather App',
+            routerDelegate: _routerDelegate,
+            routeInformationParser: _informationParser,
+            backButtonDispatcher: RootBackButtonDispatcher(),
             locale: Locale(provider.language.locale),
             localizationsDelegates: const [
               S.delegate,
---
+++ lib/router.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+// To hold app route path information.
+class AppRoutePath {
+  final int id;
+
+  AppRoutePath.start() : id = 0;
+  AppRoutePath.home() : id = 1;
+  AppRoutePath.manageLocations() : id = 2;
+  AppRoutePath.settings() : id = 3;
+
+  bool get isStart => id == 0;
+  bool get isHome => id == 1;
+  bool get isManageLocations => id == 2;
+  bool get isSettings => id == 3;
+}
+
+// Show destination page with slide animation.
+class SlidePage extends Page {
+  final Widget child;
+  final Object? args;
+  const SlidePage({LocalKey? key, required this.child, this.args}) : super(key: key);
+
+  @override
+  Route createRoute(BuildContext context) {
+    return PageRouteBuilder(
+      settings: this,
+      transitionDuration: const Duration(milliseconds: 700),
+      reverseTransitionDuration: const Duration(milliseconds: 500),
+      pageBuilder: (_, __, ___) => child,
+      transitionsBuilder: (_, animation1, animation2, child) {
+        final CurvedAnimation curvedAnimation = CurvedAnimation(
+            parent: animation1,
+            curve: Curves.fastOutSlowIn
+        );
+
+        final Animation<Offset> animation = curvedAnimation.drive(Tween<Offset>(
+            begin: const Offset(1, 0),
+            end: const Offset(0, 0)
+        ));
+
+        return SlideTransition(position: animation, child: child);
+      },
+
+    );
+  }
+}
+
+class AppRouterDelegate extends RouterDelegate<AppRoutePath> with
+    ChangeNotifier, PopNavigatorRouterDelegateMixin<AppRoutePath> {
+
+  @override
+  GlobalKey<NavigatorState>? navigatorKey;
+  AppRoutePath _currentRoute;
+  bool _isBackButtonDisabled;
+
+  // Create router.
+  AppRouterDelegate() :
+    navigatorKey = GlobalKey<NavigatorState>(),
+    _currentRoute = AppRoutePath.start(),
+    _isBackButtonDisabled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) { return false; }
+        popRoute();
+        return true;
+      },
+      pages: const []
+    );
+  }
+
+  @override
+  Future<bool> popRoute() async {
+    // :: Check for back button availability.
+    if(_isBackButtonDisabled) { return true; }
+    // :: Confirm exit app when user click on NavigationBar back button in home or start pages.
+    return _confirmExitApp();
+  }
+
+  // Navigate to any page of app.
+  void _navigateTo(AppRoutePath routePath) {
+    _currentRoute = routePath;
+    notifyListeners();
+  }
+
+  // Disable NavigationBar back button.
+  void _setBackButtonStatus(bool isDisabled) => _isBackButtonDisabled = isDisabled;
+
+  // Confirm exit app when user click on NavigationBar back button in home or start pages.
+  Future<bool> _confirmExitApp() async {
+    // TODO Ask user to exit.
+    return true;
+  }
+
+  @override
+  Future<void> setNewRoutePath(AppRoutePath configuration) async {}
+}
+
+// For parse url.
+class AppRouteInformationParser extends RouteInformationParser<AppRoutePath> {
+  @override
+  Future<AppRoutePath> parseRouteInformation(RouteInformation routeInformation) async {
+    final uri = Uri.parse(routeInformation.location!);
+    final pathSegments = uri.pathSegments;
+
+    // Handle '/'
+    if(uri.pathSegments.isEmpty) {
+      return AppRoutePath.start();
+    }
+
+    // Handle '/home'
+    if(pathSegments[0] == 'home') {
+      return AppRoutePath.home();
+    }
+
+    // Handle '/manageCities'
+    if(pathSegments[0] == 'manageCities') {
+      return AppRoutePath.manageLocations();
+    }
+
+    // Handle '/settings'
+    if(pathSegments[0] == 'settings') {
+      return AppRoutePath.settings();
+    }
+
+    return AppRoutePath.start();
+  }
+}
