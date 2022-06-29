285e47c8e2e1bd470f13a0a8570231a75ece2a3c
radnive <radnive.dev@gmail.com>
2022-06-29 12:23:37 -0700

    Fix SquareImage component.

--- lib/components/square_image.dart
+++ lib/components/square_image.dart
@@ -4,23 +4,28 @@ class SquareImage extends StatelessWidget {
   final String image;
   final Color? color;
   final double dimension;
+  final Alignment alignment;
   const SquareImage.asset(this.image, {
     Key? key,
     required this.dimension,
+    this.alignment = Alignment.center,
     this.color
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.square(
-      dimension: dimension,
-      child: Image.asset(
-        image,
-        width: dimension,
-        height: dimension,
-        fit: BoxFit.fill,
-        color: color
-      )
+    return Align(
+      alignment: alignment,
+      child: SizedBox.square(
+        dimension: dimension,
+        child: Image.asset(
+          image,
+          width: dimension,
+          height: dimension,
+          fit: BoxFit.fill,
+          color: color
+        )
+      ),
     );
   }
 }
